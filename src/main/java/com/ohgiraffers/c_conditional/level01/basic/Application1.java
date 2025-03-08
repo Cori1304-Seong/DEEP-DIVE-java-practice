e92ec246181884fb64b7ce555fb532fff065be5e
Cori1304-Seong <andyjm7739@gmail.com>
2025-03-08 16:32:21 +0900

    feat/Add answer to c_level1-01

--- src/main/java/com/ohgiraffers/c_conditional/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/c_conditional/level01/basic/Application1.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.c_conditional.level01.basic;
 
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
@@ -13,6 +15,14 @@ public class Application1 {
 		 * -- 출력 예시 --
 		 * 양수다.
 		 * */
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("정수를 하나 입력하세요: ");
+		int a = scanner.nextInt();
+
+		System.out.println(a > 0 ? "양수다." : "양수가 아니다");
+
+
 
 	}
 
