7ade4ececcdf6d99ab335d4d7c9a16357403e3fc
Cori1304-Seong <andyjm7739@gmail.com>
2025-03-08 17:27:17 +0900

    feat/Add answer to d_level2-01

--- src/main/java/com/ohgiraffers/d_looping_and_branching/level02/normal/Application1.java
+++ src/main/java/com/ohgiraffers/d_looping_and_branching/level02/normal/Application1.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.d_looping_and_branching.level02.normal;
 
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
@@ -18,6 +20,15 @@ public class Application1 {
 		 * 3 : l
 		 * 4 : e
 		 * */
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("문자열를 입력하세요 : ");
+		String str = scanner.next();
+
+		for (int i = 0; i < str.length(); i++) {
+			System.out.printf("%d : %c\n",i, str.charAt(i));
+		}
 		
 	}
 
